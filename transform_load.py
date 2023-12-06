c881bf13eb9bee8b40eb39f00ca2f9db6bc1a7c1
kasvoy <woj.kasperczyk@gmail.com>
2023-12-06 16:23:56 +0100

    working on adding constructors information to drivers table

--- transform_load.py
+++ transform_load.py
@@ -2,19 +2,24 @@ import json, datetime, psycopg2
 from sqlalchemy import create_engine
 import pandas as pd
 
+TOP_KEY = "MRData"
+RESULTS_ROOT_PATH = "data/results/"
+
 def main(): 
 
-    results = json_into_dict("data/results/22.json")
-    quali   = json_into_dict("data/qualifying/22.json")
+    results_ad = json_into_dict("data/results/22.json")
+    quali_ad   = json_into_dict("data/qualifying/22.json")
     drivers = json_into_dict("data/drivers.json")
 
     #geting a pandas dataframe from the relevant json
-    drivers_df = get_drivers_df(drivers["MRData"]["DriverTable"]["Drivers"])
+    drivers_df = get_drivers_df(drivers[TOP_KEY]["DriverTable"]["Drivers"])
+
+    #load_into_postgres(drivers_df)
+    ad_race_dict = results_ad[TOP_KEY]["RaceTable"]["Races"][0]
+    #print(drivers_df.head())
     
-    load_into_pg(drivers_df)
 
-
-def json_into_dict(file_path) -> dict:
+def json_into_dict(file_path: str) -> dict:
     with open(file_path, 'r') as f:
         dict = json.load(f)
 
@@ -25,23 +30,30 @@ def get_drivers_df(drivers_lst: list[dict]) -> pd.DataFrame:
     for drivers_dict in drivers_lst:
         drivers_dict['permanentNumber'] = int(drivers_dict['permanentNumber'])
         drivers_dict['dateOfBirth'] = datetime.datetime.strptime(drivers_dict['dateOfBirth'], "%Y-%m-%d").date()
-        del drivers_dict['url'], drivers_dict['driverId']
 
-    return pd.DataFrame(drivers_lst)
-
-
-def load_into_pg(df: pd.DataFrame):
-    engine = create_engine("postgresql+psycopg2://postgres:password@localhost:5432/test")
-    conn = psycopg2.connect("dbname=test user=postgres password=password port=5432 host=localhost")
-    cur = conn.cursor()
+    df = pd.DataFrame(drivers_lst)
+    df.drop(columns=['driverId', 'url'], inplace=True)
     
-    table_query = "CREATE TABLE drivers (perm_number INT, code VARCHAR(3), name VARCHAR(50), last_name VARCHAR(50), date_of_birth DATE, nationality VARCHAR(50));"
-    cur.execute(table_query)
-    conn.commit()
+    df.rename(columns={"permanentNumber": "permanent_number",
+                       "givenName": "first_name",
+                       "familyName": "last_name",
+                       "dateOfBirth": "dob"}, inplace=True)
+    
+    
+    df["Constructor"] = None
+    
+    race1_dict = json_into_dict(RESULTS_ROOT_PATH+"/1"+".json")
+    race1_dict = race1_dict[TOP_KEY]["RaceTable"]["Races"][0]["Results"]
+    print(race1_dict)
+    
+      
+    
+    return df
+    
+def load_into_postgres(df: pd.DataFrame):
+    engine = create_engine("postgresql+psycopg2://postgres:password@localhost:5432/test")
+    df.to_sql(name='drivers', con=engine)
 
-    #df.to_sql('drivers', engine)
-    conn.close()
-    cur.close()
     
     
 if __name__ == "__main__":
