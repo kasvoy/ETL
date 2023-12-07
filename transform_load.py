3bfcf3af910a25b5188c3ad7075e31bc24f7afd8
kasvoy <woj.kasperczyk@gmail.com>
2023-12-07 12:05:01 +0100

    Constructor info added to drivers table

--- transform_load.py
+++ transform_load.py
@@ -7,17 +7,11 @@ RESULTS_ROOT_PATH = "data/results/"
 
 def main(): 
 
-    results_ad = json_into_dict("data/results/22.json")
-    quali_ad   = json_into_dict("data/qualifying/22.json")
-    drivers = json_into_dict("data/drivers.json")
+    drivers_dict = json_into_dict("data/drivers.json")
+    drivers_df   = get_drivers_df(drivers_dict[TOP_KEY]["DriverTable"]["Drivers"])
 
-    #geting a pandas dataframe from the relevant json
-    drivers_df = get_drivers_df(drivers[TOP_KEY]["DriverTable"]["Drivers"])
+    load_into_postgres(drivers_df)
 
-    #load_into_postgres(drivers_df)
-    ad_race_dict = results_ad[TOP_KEY]["RaceTable"]["Races"][0]
-    #print(drivers_df.head())
-    
 
 def json_into_dict(file_path: str) -> dict:
     with open(file_path, 'r') as f:
@@ -27,6 +21,8 @@ def json_into_dict(file_path: str) -> dict:
 
 
 def get_drivers_df(drivers_lst: list[dict]) -> pd.DataFrame:
+    
+    #Ensure correct datatypes before loading into Postgress
     for drivers_dict in drivers_lst:
         drivers_dict['permanentNumber'] = int(drivers_dict['permanentNumber'])
         drivers_dict['dateOfBirth'] = datetime.datetime.strptime(drivers_dict['dateOfBirth'], "%Y-%m-%d").date()
@@ -40,21 +36,44 @@ def get_drivers_df(drivers_lst: list[dict]) -> pd.DataFrame:
                        "dateOfBirth": "dob"}, inplace=True)
     
     
-    df["Constructor"] = None
+    #As there is no constructor information in the Drivers json file, initiate all to None
+    df["constructor"] = None      
     
-    race1_dict = json_into_dict(RESULTS_ROOT_PATH+"/1"+".json")
-    race1_dict = race1_dict[TOP_KEY]["RaceTable"]["Races"][0]["Results"]
-    print(race1_dict)
-    
-      
+    round_no = 1
+
+    #Append constructor information from race results until all Constructor info filled
+    while df["constructor"].isna().any():
+        
+        round_no_race_dict = get_race_dict(round_no)
+        round_no_race_results: list = round_no_race_dict["Results"]
+        
+        for driver_info in round_no_race_results:
+            driver_dict      = driver_info["Driver"]
+            constructor_dict = driver_info["Constructor"]
+            
+            constructor = constructor_dict['name']
+            code        = driver_dict["code"]
+            
+            df.loc[df['code']==code, "constructor"] = constructor
+        
+        round_no += 1    
     
     return df
     
+    
 def load_into_postgres(df: pd.DataFrame):
+    
     engine = create_engine("postgresql+psycopg2://postgres:password@localhost:5432/test")
     df.to_sql(name='drivers', con=engine)
 
+
+def get_race_dict(round_no: int) -> dict:
     
+    file_suffix = f"/{str(round_no)}.json"
+    raw_dict = json_into_dict(RESULTS_ROOT_PATH + file_suffix)
+    
+    return raw_dict[TOP_KEY]["RaceTable"]["Races"][0]
+        
     
 if __name__ == "__main__":
     main()
