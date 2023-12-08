6a76ab756d4866928c2dcd4702b1d68a4e21ff5e
kasvoy <woj.kasperczyk@gmail.com>
2023-12-08 15:17:23 +0100

    loading quali

--- transform_load.py
+++ transform_load.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 TOP_KEY = "MRData"
 RESULTS_ROOT_PATH = "data/results/"
+QUALI_ROOT_PATH = "data/qualifying/"
 
 
 def main(): 
@@ -12,9 +13,10 @@ def main():
     drivers_df   = get_drivers_df(drivers_dict[TOP_KEY]["DriverTable"]["Drivers"])
     
     race_df = get_race_df()
-    load_into_postgres(df=drivers_df, table_name="drivers")
-    load_into_postgres(df=race_df, table_name="races")
-        
+    quali_df = get_quali_df()
+    #load_into_postgres(df=drivers_df, table_name="drivers")
+    #load_into_postgres(df=race_df, table_name="races")
+    #load_into_postgres(df=quali_df, table_name="qualifying")
 
 
 def json_into_dict(file_path: str) -> dict:
@@ -69,14 +71,11 @@ def get_race_df() -> pd.DataFrame:
     dflist: list[pd.DataFrame] = []
     
     for round_no in range(get_total_rounds()):
-        race_dict = get_race_dict(round_no+1)
+        race_dict = get_race_dict(round_no=round_no+1)
         results: list[dict] = race_dict["Results"]
 
-    
-        race_name    = race_dict["raceName"]
-        circuit_name = race_dict["Circuit"]["circuitName"]
-        race_date    = race_dict["date"]
-        no_laps      = results[0]["laps"]
+        (race_name, circuit_name, race_date) = get_basic_race_info(race_dict)
+        no_laps = results[0]["laps"]
         
         fastest_lap_time = None
         fastest_lap_driver_code = None
@@ -90,13 +89,33 @@ def get_race_df() -> pd.DataFrame:
                     
         fastest_lap_df = pd.DataFrame([[fastest_lap_time, fastest_lap_driver_code]], columns=["fastest_lap_time", "fastest_lap_driver"])
         info_df = pd.DataFrame([[race_name, circuit_name, race_date, no_laps]], columns=["race_name", "circuit_name", "date", "no_laps"])
-        
-        codes = [results[i]['Driver']['code'] for i in range(len(results))]
-        pos_df = pd.DataFrame([codes], columns=['P'+str(i+1) for i in range(len(results))])
+        pos_df = get_finishing_pos_df(results)
     
         dflist.append(pd.concat([info_df, pos_df, fastest_lap_df], axis=1))
     
     return pd.concat([df for df in dflist], ignore_index=True)
+
+
+def get_quali_df() -> pd.DataFrame:
+    quali_df_list = []
+    
+    for round_no in range(get_total_rounds()):
+        quali_dict = get_race_dict(round_no=round_no+1, quali=True)
+        quali_results: list[dict] = quali_dict["QualifyingResults"]
+
+        
+        (race_name, circuit_name, race_date) = get_basic_race_info(quali_dict)
+        info_df = pd.DataFrame([[race_name, circuit_name, race_date]], columns=["race_name", "circuit_name", "date"])
+        quali_pos_df = get_finishing_pos_df(quali_results)
+        
+        print(race_name)
+        for driver_info in quali_results:
+            print(driver_info.keys())
+        
+        quali_df_list.append(pd.concat([info_df, quali_pos_df], axis=1))
+        
+    
+    return pd.concat([df for df in quali_df_list], ignore_index=True)
 
 
 def load_into_postgres(df: pd.DataFrame, table_name: str):
@@ -105,17 +124,28 @@ def load_into_postgres(df: pd.DataFrame, table_name: str):
     df.to_sql(name=table_name, con=engine, index=False)
 
 
-def get_race_dict(round_no: int) -> dict:
+def get_race_dict(round_no: int, quali=False) -> dict:
     
-    file_suffix = f"/{str(round_no)}.json"
-    raw_dict = json_into_dict(RESULTS_ROOT_PATH + file_suffix)
+    file_suffix = f"{str(round_no)}.json"
+    root_path = RESULTS_ROOT_PATH if not quali else QUALI_ROOT_PATH
+    raw_dict = json_into_dict(root_path + file_suffix)
     
     return raw_dict[TOP_KEY]["RaceTable"]["Races"][0]
-
 
 def get_total_rounds():
     schedule_dict = json_into_dict("data/schedule.json")
     return int(schedule_dict[TOP_KEY]["total"])
+
+def get_basic_race_info(race_dict):
+    race_name    = race_dict["raceName"]
+    circuit_name = race_dict["Circuit"]["circuitName"]
+    race_date    = race_dict["date"]
     
+    return (race_name, circuit_name, race_date)
+
+def get_finishing_pos_df(results_dict):
+    codes = [results_dict[i]['Driver']['code'] for i in range(len(results_dict))]
+    return pd.DataFrame([codes], columns=['P'+str(i+1) for i in range(len(results_dict))])
+
 if __name__ == "__main__":
     main()
